4ae79544c0d0d666782da72c32bcf1f3c7f78834
shalini-shree <114268019+shalini-shree@users.noreply.github.com>
2022-09-27 17:05:12 -0400

    Create and read functionality

---
+++ EmployeeProfile/src/GUICmp/EmployeeType.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUICmp;
+
+/**
+ *
+ * @author Shalini Shree
+ */
+public class EmployeeType {
+    int EmployeeID;
+}
